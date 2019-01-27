db9c34f02772f5bafe1a607f4e9f5f7c22f0db45
Nemkev <ostegoelectric@gmail.com>
2019-01-27 20:24:13 +0300

    feat: add timeline

--- src/pages/autor.jsx
+++ src/pages/autor.jsx
@@ -1,5 +1,6 @@
-import React from "react"
-import Header from "../components/header"
+import React from "react";
+import Header from "../components/componentsForAutor/header";
+import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
 
 export default () => <div>
     <Header/>
