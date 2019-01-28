f33b2241e5f51e9f386a210d65c2fbd50d26a402
DmitryLenko <dias1381254@gmail.com>
2019-01-28 12:18:31 +0300

    feat: add props and change author component

--- src/pages/autor.jsx
+++ src/pages/autor.jsx
@@ -1,129 +1,30 @@
-import React from "react";
-import Header from "../components/componentsForAutor/header";
-import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
+import Header from '../components/componentsForAutor/header';
 
-export default () => <div>
-    <Header/>
-    <Timeline lineColor={'#ddd'}>
-  <TimelineItem
-    key="001"
-    dateText="11/2010 – Present"
-    style={{ color: '#e86971' }}
-  >
-    <h3>Title, Company</h3>
-    <h4>Subtitle</h4>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-  </TimelineItem>
-  <TimelineItem
-    key="002"
-    dateText="04/2009 – 11/2010"
-    dateInnerStyle={{ background: '#61b8ff', color: '#000' }}
-    bodyContainerStyle={{
-      background: '#ddd',
-      padding: '20px',
-      borderRadius: '8px',
-      boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)',
-    }}
-  >
-    <h3 style={{ color: '#61b8ff' }}>Title, Company</h3>
-    <h4 style={{ color: '#61b8ff' }}>Subtitle</h4>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-  </TimelineItem>
-  <TimelineItem
-    key="003"
-    dateComponent={(
-      <div
-        style={{
-          display: 'block',
-          float: 'left',
-          padding: '10px',
-          background: 'rgb(150, 150, 150)',
-          color: '#fff',
-        }}
-      >
-        11/2008 – 04/2009
-      </div>
-    )}
-  >
-    <h3>Title, Company</h3>
-    <h4>Subtitle</h4>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-  </TimelineItem>
-  <TimelineItem
-    key="004"
-    dateText="08/2008 – 11/2008"
-    dateInnerStyle={{ background: '#76bb7f' }}
-  >
-    <h3>Title, Company</h3>
-    <h4>Subtitle</h4>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-    <p>
-      Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
-      exercitation. Veniam velit adipisicing anim excepteur nostrud magna
-      nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-      reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis Lorem
-      est.
-    </p>
-  </TimelineItem>
-</Timeline>
-    
-</div>
+const Author = ({ location }) => {
+  const data = location.state.author;
+  return (
+    <div>
+      <Header data={data} />
+      <Timeline lineColor="#ddd">
+        {data.biography.map((biography, index) => (
+          <TimelineItem
+            key={index}
+            dateText={`${biography.year}`}
+            style={{ color: '#e86971' }}
+          >
+            <h3>{biography.event}</h3>
+          </TimelineItem>
+        ))}
+      </Timeline>
+    </div>
+  );
+};
+
+Author.propTypes = {
+  location: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default Author;
