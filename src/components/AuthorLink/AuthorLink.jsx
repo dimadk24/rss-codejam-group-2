e77a992218303b353ca514049c16f7f083873c42
DmitryLenko <dias1381254@gmail.com>
2019-01-28 12:11:01 +0300

    feat: add new prop object

--- src/components/AuthorLink/AuthorLink.jsx
+++ src/components/AuthorLink/AuthorLink.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './AuthorLink.css';
 
-function AuthorLink({ name, birthplace }) {
+function AuthorLink({ name, birthplace, data }) {
   return (
     <div className="author-link">
-      <Link to="/author">{name}.</Link>
+      <Link to="/autor/" state={{ author: data }}>
+        {name}.
+      </Link>
       <span> Город рождения: {birthplace}</span>
     </div>
   );
@@ -15,6 +17,7 @@ function AuthorLink({ name, birthplace }) {
 AuthorLink.propTypes = {
   name: PropTypes.string.isRequired,
   birthplace: PropTypes.string.isRequired,
+  data: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default AuthorLink;
