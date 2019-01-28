3e4dc14809a032daeab8329ecf660c2df5e7556c
DmitryLenko <dias1381254@gmail.com>
2019-01-28 22:43:27 +0300

    feat: add link in autor top

--- src/components/author-top.jsx
+++ src/components/author-top.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import { authors } from '../../data/authors';
 import './author-top.css';
 
@@ -14,16 +15,22 @@ const getRandomAuthor = () => {
 const AuthorsTop = () => {
   const data = getRandomAuthor();
   return (
-    <div className="author-container flex-container">
-      <img
-        className="author-container__image"
-        src={data.avatar}
-        height="300"
-        width="300"
-        alt={data.name}
-      />
-      <p className="author-container__title">{data.name}</p>
-    </div>
+    <Link
+      to="/author/"
+      state={{ author: data }}
+      className="author-container__title"
+    >
+      <div className="author-container flex-container">
+        <img
+          className="author-container__image"
+          src={data.avatar}
+          height="300"
+          width="300"
+          alt={data.name}
+        />
+        <p>{data.name}</p>
+      </div>
+    </Link>
   );
 };
 
