d0dd7b37add422be54db969541357f5ff5a0b799
DimaDK <DimaKrutDK@ya.ru>
2019-01-29 00:51:24 +0300

    feat: working overlay

--- src/components/componentsForAuthor/header.jsx
+++ src/components/componentsForAuthor/header.jsx
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './header.css';
 import ChangeLanguageSelect from '../ChangeLanguageSelect/ChangeLanguageSelect';
+import Overlay from '../youtube/overlay';
 import SliderGallery from './gallery';
 import '../../i18n';
 
-const Header = ({ data, t }) => (
-  <div>
-    <ChangeLanguageSelect />
-    <div>
-      <h1>{data.name}</h1>
-      <img src={data.avatar} alt={data.name} className="author-image" />
-    </div>
-    <h2>{t('books')}</h2>
-    <div className="author-header">
-      {data.books.map(books => (
-        <div key={books.id}>
-          <h3 className="book-name">{books.name}</h3>
-          <p className="book-data">{books.date}</p>
-          <img src={books.photo} alt={books.name} className="book-image" />
-          <button type="button">{t('buttonShowVideo')}</button>
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      overlayActive: false,
+    };
+  }
+
+  onOpenOverlay() {
+    this.setState({ overlayActive: true });
+  }
+
+  onCloseOverlay() {
+    this.setState({ overlayActive: false });
+  }
+
+  render() {
+    const { data, t } = this.props;
+    const { overlayActive } = this.state;
+    return (
+      <div>
+        <ChangeLanguageSelect />
+        <button type="button" onClick={() => this.onOpenOverlay()}>
+          {t('buttonShowVideo')}
+        </button>
+        {overlayActive && (
+          <Overlay data={data.video} onClose={() => this.onCloseOverlay()} />
+        )}
+        <div>
+          <h1>{data.name}</h1>
+          <img src={data.avatar} alt={data.name} className="author-image" />
         </div>
-      ))}
-    </div>
-    <div>
-      <SliderGallery data={data} />
-    </div>
-  </div>
-);
+        <h2>{t('books')}</h2>
+        <div className="author-header">
+          {data.books.map(books => (
+            <div key={books.id}>
+              <h3 className="book-name">{books.name}</h3>
+              <p className="book-data">{books.date}</p>
+              <img src={books.photo} alt={books.name} className="book-image" />
+            </div>
+          ))}
+        </div>
+        <div>
+          <SliderGallery data={data} />
+        </div>
+      </div>
+    );
+  }
+}
 
 Header.propTypes = {
   data: PropTypes.instanceOf(Object).isRequired,
--- src/components/youtube/overlay.jsx
+++ src/components/youtube/overlay.jsx
@@ -1,25 +1,40 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
 import './overlay.css';
 
-const Overlay = ({ data }) => {
-  const url = data.replace('watch?v=', 'embed/');
-  return (
-    <div className="overlay-container flex-container">
-      <iframe
-        type="text/html"
-        allowFullScreen="allowFullScreen"
-        src={url}
-        width="60%"
-        height="60%"
-        title="myFrame"
-      />
-    </div>
-  );
-};
+class Overlay extends Component {
+  componentDidMount() {
+    document.body.classList.add('overlay-active');
+  }
+
+  componentWillUnmount() {
+    document.body.classList.remove('overlay-active');
+  }
+
+  render() {
+    const { data, onClose } = this.props;
+    const url = data.replace('watch?v=', 'embed/');
+    return (
+      <div className="overlay-container flex-container">
+        <button type="button" onClick={onClose}>
+          X
+        </button>
+        <iframe
+          type="text/html"
+          allowFullScreen="allowFullScreen"
+          src={url}
+          width="60%"
+          height="60%"
+          title="myFrame"
+        />
+      </div>
+    );
+  }
+}
 
 Overlay.propTypes = {
   data: PropTypes.node.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
 
 export default Overlay;
