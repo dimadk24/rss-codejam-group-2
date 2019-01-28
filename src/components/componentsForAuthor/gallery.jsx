97ec96a2d94ba4f8544adcc6294a7795741f067a
DmitryLenko <dias1381254@gmail.com>
2019-01-28 21:29:14 +0300

    feat: add property in galery

--- src/components/componentsForAuthor/gallery.jsx
+++ src/components/componentsForAuthor/gallery.jsx
@@ -1,43 +1,23 @@
 import React from 'react';
 import './header.css';
 import ImageGallery from 'react-image-gallery';
-import PropTypes from 'prop-types';
 
 class SliderGallery extends React.Component {
-
-  render() {
-
-    const images = [
-      {
-        original: 'http://lorempixel.com/250/150/nature/1/',//Место для json инфы из "photosGallery"
-        thumbnail: 'http://lorempixel.com/250/150/nature/1/'//Место для json инфы из "photosGallery"
-      },
-      {
-        original: 'http://lorempixel.com/1000/600/nature/2/',//Место для json инфы из "photosGallery"
-        thumbnail: 'http://lorempixel.com/250/150/nature/2/'//Место для json инфы из "photosGallery"
-      },
-      {
-        original: 'http://lorempixel.com/1000/600/nature/3/',//Место для json инфы из "photosGallery"
-        thumbnail: 'http://lorempixel.com/250/150/nature/3/'//Место для json инфы из "photosGallery"
-      },
-      {
-        original: 'http://lorempixel.com/1000/600/nature/2/',//Место для json инфы из "photosGallery"
-        thumbnail: 'http://lorempixel.com/250/150/nature/2/'//Место для json инфы из "photosGallery"
-      },
-      {
-        original: 'http://lorempixel.com/1000/600/nature/2/',//Место для json инфы из "photosGallery"
-        thumbnail: 'http://lorempixel.com/250/150/nature/2/'//Место для json инфы из "photosGallery"
-      }
-    ]
-
-    return (
-      <ImageGallery items={images} />
+  constructor(props) {
+    super(props);
+    this.images = [];
+    this.photos = this.props;
+    this.photos.data.photosGallery.forEach(photo =>
+      this.images.push({
+        original: photo,
+        thumbnail: photo,
+      }),
     );
   }
 
+  render() {
+    return <ImageGallery items={this.images} />;
+  }
 }
 
-SliderGallery.propTypes = {
-    data: PropTypes.instanceOf(Object).isRequired,
-  };
 export default SliderGallery;
